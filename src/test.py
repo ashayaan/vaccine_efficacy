369dcc2b15f27922316550314dda1f3d84be1686
ashayaan <a.shayaan@gmail.com>
2018-09-02 16:45:05 +0530

    Linear SVC, MLP and plots

--- src/test.py
+++ src/test.py
@@ -6,9 +6,13 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import roc_curve, auc
+from sklearn.svm import SVC,LinearSVC
 import matplotlib.pyplot as plt
-import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
+import seaborn as sns
+import matplotlib
 import sys
 
 
@@ -46,23 +50,27 @@ def plotConfusionMatrix(test_labels,predicted_labels):
 class Vaccine(object):
 	seq2 = pd.Series(np.arange(2))
 
-	"""docstring for Vaccine"""
+	"""Vaccine efficacy class for training and testing various models"""
 	def __init__(self, trainFile, testFile):
 		self.trainFile = trainFile
 		self.testFile = testFile
 		self.__lr = LogisticRegression()
 		self.__dtree = DecisionTreeClassifier()
 		self.__rforest = RandomForestClassifier()
-		self.__svm = SVC(kernel='rbf')
+		self.__svm = LinearSVC(penalty='l2',random_state=0,loss='hinge',dual=True,fit_intercept=True,)
+		self.__mlp = MLPClassifier(hidden_layer_sizes=(100, ),activation='relu',solver='adam',
+								   batch_size='auto', learning_rate='constant', learning_rate_init=0.0001 )
 		self.train_data = None
 		self.train_labels = None
 		self.test_data = None
 		self.test_labels = None
 		self.predicted_labels = None
+		self.plot_data = None
 
 	def trainingData(self):
 		df = pd.read_csv(self.trainFile)
 		df = df.drop(columns='Unnamed: 0')
+		self.plot_data = df
 		df = df.dropna()
 		self.train_labels = df['label']
 		self.train_data = df.drop(columns='label')
@@ -78,6 +86,52 @@ class Vaccine(object):
 		self.trainingData()
 		self.testingData()
 
+	def plotData(self):
+		df = self.plot_data
+		fig = plt.figure()
+		ax = fig.add_subplot(111, projection='3d')
+		xs = df['1007_s_at']
+		ys = df['1320_at']
+		zs = df['200007_at']
+		colors = ['red','blue']
+		c = list(df.label)
+		ax.scatter(xs, ys, zs, c=c, s=50, alpha=0.6, edgecolors='w',cmap=matplotlib.colors.ListedColormap(colors))
+		ax.set_xlabel('1007_s_at')
+		ax.set_ylabel('1320_at')
+		ax.set_zlabel('200007_at')
+		plt.show()
+
+	@staticmethod
+	def stringLabel(col):
+		if col.label == 1:
+			return 'Effective'
+		else:
+			return 'Not Effective'
+
+	def correlationPlot(self):
+		cols = ['1007_s_at', '1053_at', '117_at', '121_at','categories']
+		df = self.plot_data
+		df['categories'] = df.apply(lambda col: self.stringLabel(col),axis=1)
+
+		pp = sns.pairplot(df[cols],hue='categories')
+		fig = pp.fig 
+		fig.subplots_adjust(top=0.93, wspace=0.3)
+		t = fig.suptitle('Genetic Attributes Pairwise Plots', fontsize=14)
+		plt.show()
+
+	def plotROC(self):
+		false_positive_rate, true_positive_rate, thresholds = roc_curve(self.test_labels, self.predicted_labels)
+		roc_auc = auc(false_positive_rate, true_positive_rate)
+		plt.title('Receiver Operating Characteristic')
+		plt.plot(false_positive_rate, true_positive_rate, color='darkorange',lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+		plt.legend(loc='lower right')
+		plt.plot([0,1],[0,1],'b--')
+		plt.xlim([-0.0,1.2])
+		plt.ylim([-0.0,1.2])
+		plt.ylabel('True Positive Rate')
+		plt.xlabel('False Positive Rate')
+		plt.show()
+
 	def trainLogisticRegression(self):
 		self.__lr.fit(self.train_data,self.train_labels)	
 	
@@ -90,7 +144,7 @@ class Vaccine(object):
 
 	def testDecesionTree(self):
 		self.predicted_labels = self.__dtree.predict(self.test_data)
-		print "Decesion Tree Score " + str(self.__dtree.score(self.test_data,self.test_labels))
+		print "Decision Tree Score " + str(self.__dtree.score(self.test_data,self.test_labels))
 	
 	def trainRandomForrest(self):
 		self.__rforest.fit(self.train_data,self.train_labels)
@@ -105,14 +159,25 @@ class Vaccine(object):
 	def testSVM(self):
 		self.predicted_labels = self.__svm.predict(self.test_data)
 		print "SVM score " + str(self.__svm.score(self.test_data,self.test_labels))
+
+	def trainMLP(self):
+		self.__mlp.fit(self.train_data,self.train_labels)
+
+	def testMLP(self):
+		self.predicted_labels = self.__mlp.predict(self.test_data)
+		print "MLP score " + str(self.__mlp.score(self.test_data,self.test_labels))
  
 if __name__ == "__main__":
 	train_data_name = sys.argv[1]
 	test_data_name = sys.argv[2]
 	model = Vaccine(train_data_name,test_data_name)
 	model.data()
+	# model.plotData()
+	# model.correlationPlot()
+
 	# model.trainLogisticRegression()
 	# model.testLogisticRegression()
+
 	# plotConfusionMatrix(model.test_labels,model.predicted_labels)
 	
 	# model.trainDecesionTree()
@@ -121,7 +186,15 @@ if __name__ == "__main__":
 	# model.trainRandomForrest()
 	# model.testRandomForrest()
 
-	model.trainSVM()
-	model.testSVM()
+	# model.trainSVM()
+	# model.testSVM()
+	
+	model.trainMLP()
+	model.testMLP()
+	
+	plotConfusionMatrix(model.test_labels,model.predicted_labels)
 
-	# plotConfusionMatrix(model.test_labels,model.predicted_labels)
+
+	model.plotROC()
+
+	
